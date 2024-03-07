130ca980e901b22ecfa13480cd1d63df6267997f
Josek94 <josekcaag94@gmail.com>
2024-03-07 12:18:50 +0100

    trabajo clase 07/03/24

--- src/repasoexamen2eva/ContadorDeMultiplos.java
+++ src/repasoexamen2eva/ContadorDeMultiplos.java
@@ -1,7 +1,5 @@
 package repasoexamen2eva;
 
-
-
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
@@ -10,64 +8,64 @@ import java.util.Set;
 
 public class ContadorDeMultiplos {
 
-	
-	public static Set<Integer> generaListaDeNumerosAleatorios(int cantidad, int maximo){
+	private static Integer[] arrayMultiplos = { 2, 3, 5, 7, 11, 13, 17 };
+
+	public static Set<Integer> generaListaDeNumerosAleatorios(int cantidad, int maximo) {
 		Set<Integer> listaDeNumeros = new HashSet<>();
 		Random random = new Random();
-		
-		while(listaDeNumeros.size() < cantidad) {
-		listaDeNumeros.add(random.nextInt(maximo)+1);  
-			
+
+		while (listaDeNumeros.size() < cantidad) {
+			listaDeNumeros.add(random.nextInt(maximo) + 1);
+
 		}
-		
-		
-		
+
 		return listaDeNumeros;
 	}
-	
-	public static Map<Integer, Integer> generarListaDeMultiplos(Set<Integer> listaDeNumeros){
+
+	public static Map<Integer, Integer> generarListaDeMultiplos2(Set<Integer> listaDeNumeros) {
 		Map<Integer, Integer> multiplosDeNumeros = new HashMap<>();
-		multiplosDeNumeros.put(2, null);
-		multiplosDeNumeros.put(3, null);
-		multiplosDeNumeros.put(5, null);
-		multiplosDeNumeros.put(7, null);
-		multiplosDeNumeros.put(11, null);
-		multiplosDeNumeros.put(13, null);
-		multiplosDeNumeros.put(17, null);
-		
-		for(Integer recorrer : listaDeNumeros) {
-			if(recorrer % 2 == 0) {
-				multiplosDeNumeros.compute(2, (key,valor)-> valor == null? 1 : valor+1);
-			} else if(recorrer % 3 == 0) {
-				multiplosDeNumeros.compute(3, (key,valor)-> valor == null? 1 : valor+1);	
-			} else if(recorrer % 5 == 0) {
-				multiplosDeNumeros.compute(5, (key,valor)-> valor == null? 1 : valor+1);	
-			} else if(recorrer % 7 == 0) {
-				multiplosDeNumeros.compute(7, (key,valor)-> valor == null? 1 : valor+1);	
-			} else if(recorrer % 11 == 0) {
-				multiplosDeNumeros.compute(11, (key,valor)-> valor == null? 1 : valor+1);	
-			} else if(recorrer % 13 == 0) {
-				multiplosDeNumeros.compute(13, (key,valor)-> valor == null? 1 : valor+1);	
-			} else if(recorrer % 17 == 0) {
-				multiplosDeNumeros.compute(17, (key,valor)-> valor == null? 1 : valor+1);	
+		listaDeNumeros.forEach(s -> {
+			for (Integer recorrer : arrayMultiplos) {
+				if (s % recorrer == 0) {
+					multiplosDeNumeros.compute(recorrer, (key, valor) -> valor == null ? 1 : valor + 1);
+				}
 			}
-		}
-		
-		
+		});
+
 		return multiplosDeNumeros;
 	}
-	
+
+	/*
+	 * public static Map<Integer, Integer> generarListaDeMultiplos(Set<Integer>
+	 * listaDeNumeros){ Map<Integer, Integer> multiplosDeNumeros = new HashMap<>();
+	 * multiplosDeNumeros.put(2, null); multiplosDeNumeros.put(3, null);
+	 * multiplosDeNumeros.put(5, null); multiplosDeNumeros.put(7, null);
+	 * multiplosDeNumeros.put(11, null); multiplosDeNumeros.put(13, null);
+	 * multiplosDeNumeros.put(17, null);
+	 * 
+	 * for(Integer recorrer : listaDeNumeros) { if(recorrer % 2 == 0) {
+	 * multiplosDeNumeros.compute(2, (key,valor)-> valor == null? 1 : valor+1); }
+	 * else if(recorrer % 3 == 0) { multiplosDeNumeros.compute(3, (key,valor)->
+	 * valor == null? 1 : valor+1); } else if(recorrer % 5 == 0) {
+	 * multiplosDeNumeros.compute(5, (key,valor)-> valor == null? 1 : valor+1); }
+	 * else if(recorrer % 7 == 0) { multiplosDeNumeros.compute(7, (key,valor)->
+	 * valor == null? 1 : valor+1); } else if(recorrer % 11 == 0) {
+	 * multiplosDeNumeros.compute(11, (key,valor)-> valor == null? 1 : valor+1); }
+	 * else if(recorrer % 13 == 0) { multiplosDeNumeros.compute(13, (key,valor)->
+	 * valor == null? 1 : valor+1); } else if(recorrer % 17 == 0) {
+	 * multiplosDeNumeros.compute(17, (key,valor)-> valor == null? 1 : valor+1); } }
+	 * 
+	 * 
+	 * return multiplosDeNumeros; }
+	 */
 	public static void main(String[] args) {
-		
-		
-		
-		Set<Integer> listaDeNumeros1 = generaListaDeNumerosAleatorios(100, 1000);
-		
-		Map<Integer, Integer> listaDeMultiplos = generarListaDeMultiplos(listaDeNumeros1);
-		
-		listaDeMultiplos.forEach((key,valor) -> System.out.println("key: " + key + " valor: " + valor));
-		
+
+		Set<Integer> listaDeNumeros1 = generaListaDeNumerosAleatorios(30, 1000);
+
+		Map<Integer, Integer> listaDeMultiplos = generarListaDeMultiplos2(listaDeNumeros1);
+
+		listaDeMultiplos.forEach((key, valor) -> System.out.println("key: " + key + " valor: " + valor));
+
 	}
-	
-	
+
 }
--- src/repasoexamen2eva/ElementoQuimico.java
+++ src/repasoexamen2eva/ElementoQuimico.java
@@ -1,9 +1,85 @@
 package repasoexamen2eva;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Random;
+import java.util.Set;
+
 public class ElementoQuimico {
 
+	private String simbolo;
+	private String nombreElemento;
+	private int numeroAtomico;
+	private static Random random = new Random();
+	//private static List<ElementoQuimico> listaDeElementos = new ArrayList<>();
+	private static Set<ElementoQuimico> setDeElementos = new HashSet<>
+	(Set.of(new ElementoQuimico("H", "Hidrógeno", 1),
+            new ElementoQuimico("He", "Helio", 2),
+            new ElementoQuimico("Li", "Litio", 3),
+            new ElementoQuimico("Be", "Berilio", 4),
+            new ElementoQuimico("B", "Boro", 5),
+            new ElementoQuimico("C", "Carbono", 6),
+            new ElementoQuimico("N", "Nitrógeno", 7),
+            new ElementoQuimico("O", "Oxígeno", 8),
+            new ElementoQuimico("F", "Flúor", 9),
+            new ElementoQuimico("Ne", "Neón", 10),
+            new ElementoQuimico("Na", "Sodio", 11),
+            new ElementoQuimico("Mg", "Magnesio", 12),
+            new ElementoQuimico("Al", "Aluminio", 13),
+            new ElementoQuimico("Si", "Silicio", 14),
+            new ElementoQuimico("P", "Fósforo", 15),
+            new ElementoQuimico("S", "Azufre", 16),
+            new ElementoQuimico("Cl", "Cloro", 17),
+            new ElementoQuimico("Ar", "Argón", 18),
+            new ElementoQuimico("K", "Potasio", 19),
+            new ElementoQuimico("Ca", "Calcio", 20)));
+	
+	
+	public ElementoQuimico(String simbolo, String nombreElemento, int numeroAtomico) {
+		this.simbolo = simbolo;
+		this.nombreElemento = nombreElemento;
+		this.numeroAtomico = numeroAtomico;
+	}
 
 	
 	
+
 	
+	
+	@Override
+	public String toString() {
+		return "ElementoQuimico [simbolo=" + simbolo + ", nombreElemento=" + nombreElemento + ", numeroAtomico="
+				+ numeroAtomico + "]";
+	}
+
+
+
+
+
+
+	public static ElementoQuimico[] generarElementosAleatorios(int cantidad) {
+		List<ElementoQuimico> listaDeElementosQuimicos = new ArrayList<>(setDeElementos);
+		Collections.shuffle(listaDeElementosQuimicos);
+		ElementoQuimico[] arrayDeElementos = new ElementoQuimico[cantidad];
+		for(int i = 0; i < cantidad;i++) {
+			arrayDeElementos[i] = listaDeElementosQuimicos.get(i);
+			
+		}
+		
+		return arrayDeElementos;
+	}
+	
+	public static void main(String[] args) {
+		
+		for(ElementoQuimico recorrer : generarElementosAleatorios(10)) {
+			System.out.println(recorrer);
+		}
+		
+		
+		
+		
+		
+	}
 }
